59131d6a6669a74698ddebad2839a717e17a1c36
Renzie <omanarenzie@gmail.com>
2020-03-23 11:10:00 +0100

    added calculator

--- app/router.js
+++ app/router.js
@@ -7,4 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('numbers',{path: "/"}, function() {
+    this.route('remove', {path: '/remove/:id'});
+  });
 });
